f9178655551d315ca6fdec439e25c3b6d0ea5392
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 12:46:21 +0530

    Add solved combination_sum_1752390974.py

---
+++ AutoRep1/combination_sum_1752390974.py
@@ -0,0 +1,10 @@
+"""
+Problem: Combination Sum
+Description: Write a function to solve the combination sum problem.
+"""
+
+def combination_sum(input_data):
+    # Implement a function to solve the combination sum problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
