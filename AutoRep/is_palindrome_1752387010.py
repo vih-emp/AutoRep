e32dd452374a4110ef5958f307e87948261fdc9f
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 11:40:23 +0530

    Add solved is_palindrome_1752387010.py

---
+++ AutoRep/is_palindrome_1752387010.py
@@ -0,0 +1,9 @@
+"""
+Problem: Is Palindrome
+Description: Given a string s, return True if it is a palindrome, False otherwise.
+"""
+
+def is_palindrome(s):
+                        # Implement a function that returns True if s is a palindrome, False otherwise.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+    pass
