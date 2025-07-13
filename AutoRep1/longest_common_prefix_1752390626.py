cd7b8afe2d4e26c3e20ca608cdaaaa41acbd61ad
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 12:40:33 +0530

    Add solved longest_common_prefix_1752390626.py

---
+++ AutoRep1/longest_common_prefix_1752390626.py
@@ -0,0 +1,10 @@
+"""
+Problem: Longest Common Prefix
+Description: Write a function to solve the longest common prefix problem.
+"""
+
+def longest_common_prefix(input_data):
+    # Implement a function to solve the longest common prefix problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
