a17a097c08ed3177ac8aadd47631c75cf7bc60c1
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:13:14 +0530

    Add solved two_sum_1752392586.py

---
+++ AutoRep1/two_sum_1752392586.py
@@ -0,0 +1,10 @@
+"""
+Problem: Two Sum
+Description: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+"""
+
+def two_sum(nums, target):
+    # Implement a function that returns the indices of two numbers that add up to the target.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
