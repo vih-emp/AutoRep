c462cf37c20729cde8bc0e98b64fe76d63a27001
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:02:39 +0530

    Add solved symmetric_tree_1752391951.py

---
+++ AutoRep1/symmetric_tree_1752391951.py
@@ -0,0 +1,10 @@
+"""
+Problem: Symmetric Tree
+Description: Write a function to solve the symmetric tree problem.
+"""
+
+def symmetric_tree(input_data):
+    # Implement a function to solve the symmetric tree problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
