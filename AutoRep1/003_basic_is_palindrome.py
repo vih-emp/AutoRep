53295673ba4ec2d68a06d5728057f6ff8b0d0d5a
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:35:04 +0530

    Add solved 003_basic_is_palindrome.py

---
+++ AutoRep1/003_basic_is_palindrome.py
@@ -0,0 +1,11 @@
+def is_palindrome(input_data):
+    """
+    Topic 3: Is Palindrome (Basic)
+    Implement a function to solve the "is palindrome" problem.
+    Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+    """
+    #Program implementation given below:
+
+    # Your code here
+
+    pass
