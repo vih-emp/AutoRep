19b5752e400a34a3211b38b41f43b0361373778f
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:49:02 +0530

    Add solved 006_advanced_unique_binary_search_trees.py

---
+++ AutoRep1/006_advanced_unique_binary_search_trees.py
@@ -0,0 +1,11 @@
+def unique_binary_search_trees(input_data):
+    """
+    Topic 6: Unique Binary Search Trees (Advanced)
+    Implement a function to solve the "unique binary search trees" problem.
+    Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+    """
+    #Program implementation given below:
+
+    # Your code here    
+
+    pass
