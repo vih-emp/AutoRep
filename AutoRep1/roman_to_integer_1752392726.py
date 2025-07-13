727e848ab58c6cd8347ad6593a7e10ee7c72a37a
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:15:33 +0530

    Add solved roman_to_integer_1752392726.py

---
+++ AutoRep1/roman_to_integer_1752392726.py
@@ -0,0 +1,10 @@
+"""
+Problem: Roman To Integer
+Description: Write a function to solve the roman to integer problem.
+"""
+
+def roman_to_integer(input_data):
+    # Implement a function to solve the roman to integer problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
