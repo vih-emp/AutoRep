f7341a6c80a085e3a6003830c5a74102a35549be
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 12:42:52 +0530

    Add solved fizz_buzz_1752390765.py

---
+++ AutoRep1/fizz_buzz_1752390765.py
@@ -0,0 +1,10 @@
+"""
+Problem: Fizz Buzz
+Description: Print numbers from 1 to n. For multiples of 3 print 'Fizz', for multiples of 5 print 'Buzz', and for numbers which are multiples of both print 'FizzBuzz'.
+"""
+
+def fizz_buzz(n):
+    # Implement a function that prints numbers from 1 to n with FizzBuzz logic.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
