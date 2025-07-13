1875507340ed6a31caefe58708e2895b6b6c1dd5
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:34:43 +0530

    Add solved 001_basic_two_sum.py

---
+++ AutoRep1/001_basic_two_sum.py
@@ -0,0 +1,11 @@
+def two_sum(input_data):
+    """
+    Topic 1: Two Sum (Basic)
+    Implement a function to solve the "two sum" problem.
+    Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+    """
+    #Program implementation given below:
+
+    # Your code here
+
+    pass
