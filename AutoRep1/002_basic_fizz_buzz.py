52eeecbcbc4f52cb9dd087c954fc9fedcc23309e
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:34:54 +0530

    Add solved 002_basic_fizz_buzz.py

---
+++ AutoRep1/002_basic_fizz_buzz.py
@@ -0,0 +1,11 @@
+def fizz_buzz(input_data):
+    """
+    Topic 2: Fizz Buzz (Basic)
+    Implement a function to solve the "fizz buzz" problem.
+    Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+    """
+    #Program implementation given below:
+
+    # Your code here
+
+    pass
