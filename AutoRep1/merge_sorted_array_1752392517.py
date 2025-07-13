e3853129bb88390046ccc73fcd74c276c1ae707e
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:12:04 +0530

    Add solved merge_sorted_array_1752392517.py

---
+++ AutoRep1/merge_sorted_array_1752392517.py
@@ -0,0 +1,10 @@
+"""
+Problem: Merge Sorted Array
+Description: Write a function to solve the merge sorted array problem.
+"""
+
+def merge_sorted_array(input_data):
+    # Implement a function to solve the merge sorted array problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
