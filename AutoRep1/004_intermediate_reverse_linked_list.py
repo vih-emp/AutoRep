650bd42317a84c219f09f24b88866c5ea05dae88
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:35:14 +0530

    Add solved 004_intermediate_reverse_linked_list.py

---
+++ AutoRep1/004_intermediate_reverse_linked_list.py
@@ -0,0 +1,11 @@
+def reverse_linked_list(input_data):
+    """
+    Topic 4: Reverse Linked List (Intermediate)
+    Implement a function to solve the "reverse linked list" problem.
+    Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+    """
+    #Program implementation given below:
+
+    # Your code here
+
+    pass
