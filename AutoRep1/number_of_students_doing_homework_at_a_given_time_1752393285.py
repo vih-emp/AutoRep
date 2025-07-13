da6bb8ba77f712ec4e3a046c44431f96051f1295
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:24:53 +0530

    Add solved number_of_students_doing_homework_at_a_given_time_1752393285.py

---
+++ AutoRep1/number_of_students_doing_homework_at_a_given_time_1752393285.py
@@ -0,0 +1,10 @@
+"""
+Problem: Number Of Students Doing Homework At A Given Time
+Description: Write a function to solve the number of students doing homework at a given time problem.
+"""
+
+def number_of_students_doing_homework_at_a_given_time(input_data):
+    # Implement a function to solve the number of students doing homework at a given time problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
