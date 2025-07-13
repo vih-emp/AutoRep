28fb8db31874f420d1535bd41d560e8fbcc7acac
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 12:51:01 +0530

    Add solved group_anagrams_1752391253.py

---
+++ AutoRep1/group_anagrams_1752391253.py
@@ -0,0 +1,10 @@
+"""
+Problem: Group Anagrams
+Description: Write a function to solve the group anagrams problem.
+"""
+
+def group_anagrams(input_data):
+    # Implement a function to solve the group anagrams problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
