0d77f25e4a6f365cceda3914a8bc2eead2219138
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:03:50 +0530

    Add solved robot_return_to_origin_1752392022.py

---
+++ AutoRep1/robot_return_to_origin_1752392022.py
@@ -0,0 +1,10 @@
+"""
+Problem: Robot Return To Origin
+Description: Write a function to solve the robot return to origin problem.
+"""
+
+def robot_return_to_origin(input_data):
+    # Implement a function to solve the robot return to origin problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
