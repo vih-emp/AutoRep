8a599b44ff63b90d8754c31c017a613e1b2bd5f6
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 12:28:56 +0530

    Add solved difference_of_squares_1752389928.py

---
+++ AutoRep1/difference_of_squares_1752389928.py
@@ -0,0 +1,10 @@
+"""
+Problem: Difference Of Squares
+Description: Write a function to solve the difference of squares problem.
+"""
+
+def difference_of_squares(input_data):
+    # Implement a function to solve the difference of squares problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
