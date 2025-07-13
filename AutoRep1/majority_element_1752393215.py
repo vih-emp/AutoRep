1a6252f8e9d07ad798c709a55ce6a7e9f7e4b463
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:23:43 +0530

    Add solved majority_element_1752393215.py

---
+++ AutoRep1/majority_element_1752393215.py
@@ -0,0 +1,10 @@
+"""
+Problem: Majority Element
+Description: Write a function to solve the majority element problem.
+"""
+
+def majority_element(input_data):
+    # Implement a function to solve the majority element problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
