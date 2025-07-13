7289c65f9cf39a6e04c498ce9d3e60f2cf25471c
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:22:32 +0530

    Add solved binary_tree_preorder_traversal_1752393144.py

---
+++ AutoRep1/binary_tree_preorder_traversal_1752393144.py
@@ -0,0 +1,10 @@
+    """
+Problem: Binary Tree Preorder Traversal
+Description: Write a function to solve the binary tree preorder traversal problem.
+"""
+
+def binary_tree_preorder_traversal(input_data):
+    # Implement a function to solve the binary tree preorder traversal problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
