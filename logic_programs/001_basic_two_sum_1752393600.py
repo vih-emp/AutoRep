c148075ed3c37f5783290786703abe238b3d6b72
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:30:08 +0530

    Add solved 001_basic_two_sum_1752393600.py

---
+++ logic_programs/001_basic_two_sum_1752393600.py
@@ -0,0 +1,9 @@
+def two_sum(input_data):
+    """
+    Topic 1: Two Sum (Basic)
+    Implement a function to solve the "two sum" problem.
+    Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+    """
+    # Your code here
+
+    pass
