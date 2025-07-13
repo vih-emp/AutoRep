3c5faf2d068a88b2f3aa2b48ff9df2d75e4b87e9
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:21:22 +0530

    Add solved find_peak_element_1752393075.py

---
+++ AutoRep1/find_peak_element_1752393075.py
@@ -0,0 +1,10 @@
+"""
+Problem: Find Peak Element
+Description: Write a function to solve the find peak element problem.
+"""
+
+def find_peak_element(input_data):
+    # Implement a function to solve the find peak element problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
