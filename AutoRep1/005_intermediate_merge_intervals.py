94fd95b05d4d11c47dde15b8ed23c2e8826a3f22
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:48:46 +0530

    Add solved 005_intermediate_merge_intervals.py

---
+++ AutoRep1/005_intermediate_merge_intervals.py
@@ -0,0 +1,11 @@
+def merge_intervals(input_data):
+    """
+    Topic 5: Merge Intervals (Intermediate)
+    Implement a function to solve the "merge intervals" problem.
+    Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+    """
+    #Program implementation given below:
+
+    # Your code here    
+
+    pass
