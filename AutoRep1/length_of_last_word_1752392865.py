8aac4a99477eff92b3bb27780b081379bee7de22
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:17:53 +0530

    Add solved length_of_last_word_1752392865.py

---
+++ AutoRep1/length_of_last_word_1752392865.py
@@ -0,0 +1,10 @@
+"""
+Problem: Length Of Last Word
+Description: Write a function to solve the length of last word problem.
+"""
+
+def length_of_last_word(input_data):
+    # Implement a function to solve the length of last word problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
