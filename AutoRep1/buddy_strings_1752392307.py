7ef3b9dca8ed46a1cae0ffb4481f94f07d17ba35
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:08:35 +0530

    Add solved buddy_strings_1752392307.py

---
+++ AutoRep1/buddy_strings_1752392307.py
@@ -0,0 +1,10 @@
+"""
+Problem: Buddy Strings
+Description: Write a function to solve the buddy strings problem.
+"""
+
+def buddy_strings(input_data):
+    # Implement a function to solve the buddy strings problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
