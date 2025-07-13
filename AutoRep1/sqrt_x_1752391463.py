7556028dabd0934bbbfa096681eea55e56bca785
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 12:54:30 +0530

    Add solved sqrt_x_1752391463.py

---
+++ AutoRep1/sqrt_x_1752391463.py
@@ -0,0 +1,10 @@
+"""
+Problem: Sqrt X
+Description: Write a function to solve the sqrt x problem.
+"""
+
+def sqrt_x(input_data):
+    # Implement a function to solve the sqrt x problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
