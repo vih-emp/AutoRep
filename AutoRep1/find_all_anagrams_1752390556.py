044bfa367283b79b84b4ad6738116f9176bb2ce0
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 12:39:23 +0530

    Add solved find_all_anagrams_1752390556.py

---
+++ AutoRep1/find_all_anagrams_1752390556.py
@@ -0,0 +1,10 @@
+"""
+Problem: Find All Anagrams
+Description: Write a function to solve the find all anagrams problem.
+"""
+
+def find_all_anagrams(input_data):
+    # Implement a function to solve the find all anagrams problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
