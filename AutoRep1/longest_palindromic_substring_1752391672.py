136e89d1d53044bfb0fbe0f8bea72afac8f6f2c4
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 12:57:59 +0530

    Add solved longest_palindromic_substring_1752391672.py

---
+++ AutoRep1/longest_palindromic_substring_1752391672.py
@@ -0,0 +1,10 @@
+"""
+Problem: Longest Palindromic Substring
+Description: Write a function to solve the longest palindromic substring problem.
+"""
+
+def longest_palindromic_substring(input_data):
+    # Implement a function to solve the longest palindromic substring problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
