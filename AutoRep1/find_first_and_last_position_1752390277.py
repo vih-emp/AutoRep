b0cb08f0064d5501d0a4d3398045c06d98f40f23
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 12:34:44 +0530

    Add solved find_first_and_last_position_1752390277.py

---
+++ AutoRep1/find_first_and_last_position_1752390277.py
@@ -0,0 +1,10 @@
+"""
+Problem: Find First And Last Position
+Description: Write a function to solve the find first and last position problem.
+"""
+
+def find_first_and_last_position(input_data):
+    # Implement a function to solve the find first and last position problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
