a336deb42cefac6ec2a5e1851f5c8e116028cbe3
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 13:10:54 +0530

    Add solved merge_intervals_1752392447.py

---
+++ AutoRep1/merge_intervals_1752392447.py
@@ -0,0 +1,10 @@
+"""
+Problem: Merge Intervals
+Description: Write a function to solve the merge intervals problem.
+"""
+
+def merge_intervals(input_data):
+    # Implement a function to solve the merge intervals problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
