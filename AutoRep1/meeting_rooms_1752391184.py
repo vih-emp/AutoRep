b807b44686c371a89d015e44f8f3ad104b67cfbf
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 12:49:51 +0530

    Add solved meeting_rooms_1752391184.py

---
+++ AutoRep1/meeting_rooms_1752391184.py
@@ -0,0 +1,10 @@
+"""
+Problem: Meeting Rooms
+Description: Write a function to solve the meeting rooms problem.
+"""
+
+def meeting_rooms(input_data):
+    # Implement a function to solve the meeting rooms problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
