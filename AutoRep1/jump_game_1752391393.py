dd8ffcd0df22a6e61c987d34f39b9b49162d8761
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 12:53:20 +0530

    Add solved jump_game_1752391393.py

---
+++ AutoRep1/jump_game_1752391393.py
@@ -0,0 +1,10 @@
+"""
+Problem: Jump Game
+Description: Write a function to solve the jump game problem.
+"""
+
+def jump_game(input_data):
+    # Implement a function to solve the jump game problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
