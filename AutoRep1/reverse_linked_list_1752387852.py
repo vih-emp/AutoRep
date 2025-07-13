f0a9449dbf453dc42d2822bb3cfc5ff1e1017549
Nathan Pinto <vihaan.nathan@gmail.com>
2025-07-13 11:54:20 +0530

    Add solved reverse_linked_list_1752387852.py

---
+++ AutoRep1/reverse_linked_list_1752387852.py
@@ -0,0 +1,10 @@
+"""
+Problem: Reverse Linked List
+Description: Write a function to solve the reverse linked list problem.
+"""
+
+def reverse_linked_list(input_data):
+    # Implement a function to solve the reverse linked list problem.
+    # Write the exact code to solve this problem. When finished, append the comment #Done! on the last line.
+
+    pass
